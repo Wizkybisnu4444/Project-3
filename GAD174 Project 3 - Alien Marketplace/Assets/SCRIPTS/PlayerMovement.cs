fbf5d1ecc33a7ba3ad3473d7128471cbdeedab07
AlecDubouzet <85209846+wrobe71279@users.noreply.github.com>
2021-08-11 22:57:32 +1000

    Updated PlayerMovement
    - Added gravity
    - Added jump

--- GAD174 Project 3 - Alien Marketplace/Assets/SCRIPTS/PlayerMovement.cs
+++ GAD174 Project 3 - Alien Marketplace/Assets/SCRIPTS/PlayerMovement.cs
@@ -7,15 +7,41 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float speed = 12f;
+    public float gravity = 9.81f;
+    public float jumpHeight = 3f;
 
     public float turnSmoothTime = 0.1f;
 
     float turnSmoothVelocity;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    Vector3 velocity;
+
     // Update is called once per frame
     void Update()
     {
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravity);
+        }
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -28,6 +54,10 @@ public class PlayerMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
         }
+
+        
+
     }
 }
